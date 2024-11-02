1f06601013cf3b0094a11446cae8a6b9fc8d95e8
mivui <49021590+mivui@users.noreply.github.com>
2024-11-02 16:19:59 +0800

    fix: type

--- lib/env.d.ts
+++ lib/env.d.ts
@@ -1,13 +1,3 @@
-declare module '@eslint/js' {
-  import { type Linter } from 'eslint';
-  declare const configs: {
-    all: Linter.Config;
-    recommended: Linter.Config;
-  };
-
-  export { configs };
-}
-
 declare module 'eslint-config-prettier' {
   import { type Linter } from 'eslint';
   declare const config: Linter.Config;
